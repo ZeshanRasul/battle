0a514463a2710d531b60aab4e511641f4b6e23af
Makers Academy <makers@Makerss-MacBook-Pro-2.local>
2016-02-05 11:45:17 +0000

    Added multiplayer

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,10 @@
 class Game
-  attr_reader :player_1, :player_2, :current_turn
+  attr_reader :player_1, :player_2, :current_turn, :opponent
 
   def initialize(player_1, player_2)
     @players = [player_1, player_2]
     @current_turn = player_1
+    @opponent = player_2
   end
 
   def player_1
@@ -19,10 +20,12 @@ class Game
   end
 
   def switch_turns
-    if @current_turn = player_1
+    if @current_turn == player_1
       @current_turn = player_2
+      @opponent = player_1
     else
       @current_turn = player_1
+      @opponent = player_2
     end
-  end 
+  end
 end
--- spec/features/hitpoints_spec.rb
+++ spec/features/hitpoints_spec.rb
@@ -4,5 +4,10 @@ feature 'Viewing hitpoints' do
     scenario "Players can view other players hitpoints" do
       sign_in_and_play
       expect(page).to have_content("Lou: 100hp")
-  end
+    end
+
+    scenario 'see player 2s hitpoints' do
+      sign_in_and_play
+      expect(page).to have_content("Zee: 100hp")
+    end 
 end
